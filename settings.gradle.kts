e34b51230b7de45a57f9b4682d9daa2afb062a6d
Jessica, Monica e Vinicius <jeferreira.ti@gmail.com>
2021-01-10 20:51:32 -0300

    chore: init repository

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "passkey-validation"
---
+++ src/test/kotlin/com/stephanogiuseppe/passkeyvalidation/PasskeyValidationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stephanogiuseppe.passkeyvalidation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PasskeyValidationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
